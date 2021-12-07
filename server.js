f2aac32e7d651dc225dcc3feea73fd3251524cbf
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-12-07 15:49:00 -0500

    Added voters table

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.get('/api/candidates', (req, res) => {
     });
   });
 
+  
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
     const sql = `SELECT candidates.*, parties.name 
